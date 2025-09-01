ee425d88f743fcbc979847e5b8bb9687aa1102a0
Dan Chao <dan.chao@apple.com>
2025-08-31 22:26:33 -0700

    Make fake PR

--- src/test/kotlin/org/pkl/intellij/annotator/PklHighlightingAnnotatorTest.kt
+++ src/test/kotlin/org/pkl/intellij/annotator/PklHighlightingAnnotatorTest.kt
@@ -1,5 +1,5 @@
 /**
- * Copyright © 2024 Apple Inc. and the Pkl project authors. All rights reserved.
+ * Copyright © 2024-2025 Apple Inc. and the Pkl project authors. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -78,6 +78,11 @@ internal class PklHighlightingAnnotatorTest {
   fun after() {
     codeInsightTestFixture.tearDown()
     PklAnnotator.enabledTestAnnotator = null
+  }
+
+  @Test
+  fun `fake failure`() {
+    require(false) { "This is a fake failure" }
   }
 
   @Test
